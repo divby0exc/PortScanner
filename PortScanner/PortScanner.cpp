7dab0aa8606c9f896d089e182d6e13d7d9b21246
Daniel Sedigh <seddigh.daniel@gmail.com>
2024-06-10 15:53:46 +0200

    added ports and exception handling within the loop to test them all

--- PortScanner/PortScanner.cpp
+++ PortScanner/PortScanner.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
-#include <Poco/Net/Socket.h>
 #include <Poco/Net/StreamSocket.h>
+#include <vector>
+#include <string>
 
 int main() {
-	Poco::Net::IPAddress address("127.0.0.1");
-	Poco::Net::SocketAddress socket_address("127.0.0.1:8080");
-	Poco::Net::SocketAddress socket_address2("127.0.0.1", 8080);
+	std::vector<std::string> ports{ "20","21","22","23","25","53","137","139","445","80","443","8080","8443","1433","1434","3306","3389" };
 
-	try {
-	Poco::Net::StreamSocket socket(socket_address2);
-	socket.connect(socket_address2);
-	socket.close();
 
+	for (size_t i{ 0 }; i < ports.size(); ++i)
+	{
+		try
+		{
+			Poco::Net::StreamSocket socket(Poco::Net::SocketAddress("127.0.0.1",ports[i]));
+			std::cout << ports[i] << " is OPEN\n";
+			socket.close();
+		}
+		catch (Poco::Exception& exc)
+		{
+			std::cout << "Port: " << ports[i] << " is CLOSED\n";
+		}
 	}
-	catch (Poco::Exception& exc) {
-		std::cout << exc.displayText() << "\n";
-	}
 
-
-
-	
 
 	return 0;
 }
